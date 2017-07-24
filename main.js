46c265cdfc1c153394daba234c112b22d5d42ff5
Megha Bhargava <er.meghabhargava@gmail.com>
2017-07-24 16:24:32 -0700

    add 5 restaurtant and search bar

--- main.js
+++ main.js
@@ -13,17 +13,18 @@ $('#user-email').on('input',function() {
 		//	$scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
 
 $scope.restaurants = [{
-	name: 'Farzi Cafe',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-	location: 'Connaught Place',
+	name: 'Laksh Restaurant',
+	address: 'Sai Road',
+	location: 'Baddi',
 	category: 'Casual Dining, Bar',
-	vote: '4.2',
+	vote: '4.5',
 	cuisines: 'Modern Indian',
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+	cost: '600',
+	hours: '9 AM to 10 PM (Mon-Sun)',
+	image: 'https://asia-public.foodpanda.com/assets/production/in/images/vendors/r2xc.jpg?v=20170610153153'
 },
-{name: 'A1 dhaba',
+{
+	name: 'A1 dhaba',
 	address: 'bhud',
 	location: 'baddi',
 	category: 'Casual Dining, Bar',
@@ -32,5 +33,38 @@ $scope.restaurants = [{
 	cost: '1200',
 	hours: '9 AM to 10 PM (Mon-Sun)',
 	image: 'https://www.topranker4u.com/wp-content/uploads/2017/04/top-dhaba-restaurants-300-200-300x200.jpg'
+},
+{
+	name: 'Dawat Restaurant',
+	address: 'Near Canara Bank,Rotary Chowk,Sai road',
+	location: 'Baddi',
+	category: 'Casual Dining, Bar',
+	vote: '4',
+	cuisines: 'Modern Indian & chinease',
+	cost: '1500',
+	hours: '9:30AM to 10:30PM (Mon-Sun)',
+	image: 'http://images.mydala.com/uploads/event/2015-06-02/217112/217112_1.jpg'
+},
+{
+	name: 'Saffron Restaurant',
+	address: 'MDR7',
+	location: 'baddi',
+	category: 'Casual Dining,Sweets & Bakers',
+	vote: '3.9',
+	cuisines: 'Modern Indian',
+	cost: '500',
+	hours: '7:30AM to 10 PM (Mon-Sun)',
+	image: 'https://qph.ec.quoracdn.net/main-qimg-c50293086be900edec89867b17958da8-c'
+},
+{
+	name: 'Ananda Bhawan',
+	address: 'SCF 4,Phase 1,Housing Board,Sai Road,Near Old Malhorta Hospital',
+	location: 'baddi',
+	category: 'Casual Dining, Bar',
+	vote: '4.1',
+	cuisines: 'Modern Indian & south indian',
+	cost: '2000',
+	hours: '7:30AM to 10:30 PM (Mon-Sun)',
+	image: 'http://anandabhavanbaddi.com/images/client-1.jpg'
 }]
 })
