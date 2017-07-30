df93ec5b7fd9a19726c31273f08aa8218b2e24d3
Megha Bhargava <er.meghabhargava@gmail.com>
2017-07-30 07:32:11 -0700

    add background images in every file and modify data of favourite file

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ $scope.restaurants = [{
 }]
 })
 
-// favController
+//------------------------------ favController
 
 foodieApp.controller('favController',function($scope,$location,$http) {
 
@@ -227,30 +227,30 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 //console.log($routeParams.id);
 
   $scope.restaurants = [{
-							id: 1,
-							bestDish: {
-										name: 'Cake',
-										image: 'http://img10.deviantart.net/c2a3/i/2015/165/0/2/small_ckae_by_mariosonicfan16-d8x8b0c.jpg'
-									},
-							image: 'http://img10.deviantart.net/c2a3/i/2015/165/0/2/small_ckae_by_mariosonicfan16-d8x8b0c.jpg'
-						},{
-
+    							id: 1,
+    							bestDish: {
+    										name: 'Cake',
+    										image: 'http://img10.deviantart.net/c2a3/i/2015/165/0/2/small_ckae_by_mariosonicfan16-d8x8b0c.jpg'
+    									},
+    							image: 'http://img10.deviantart.net/c2a3/i/2015/165/0/2/small_ckae_by_mariosonicfan16-d8x8b0c.jpg'
+    						},
+            {
 						id: 2,
 						bestDish: {
-									name: 'Corn Pizza',
-									image: 'https://images.food52.com/zyrGNL1_8ZxmJ43jtBLQoyLfNvA=/753x502/c947f605-2d61-4a10-9f69-abc7dda9fffb--DSC07406.JPG'
+									name: 'Panner Pizza',
+									image: 'http://vegkhanakhazana.in/wp-content/uploads/2017/02/paneer-pizza1.jpg'
 								},
 
-            image: 'https://images.food52.com/zyrGNL1_8ZxmJ43jtBLQoyLfNvA=/753x502/c947f605-2d61-4a10-9f69-abc7dda9fffb--DSC07406.JPG'
+            image: 'http://vegkhanakhazana.in/wp-content/uploads/2017/02/paneer-pizza1.jpg'
           },
           {
 								id: 3,
 								bestDish: {
-											name: 'Italian Pasta',
-											image: 'https://www.sensibus.com/deli/sites/sensibus.com/files/recipes/pasta-dish-2_0.jpg'
+											name: 'Sandwich',
+											image: 'http://domemanila.com/wp-content/uploads/2015/09/sandwich-club-sandwich.jpg'
 										},
 
-                image: 'https://www.sensibus.com/deli/sites/sensibus.com/files/recipes/pasta-dish-2_0.jpg'
+                image: 'http://domemanila.com/wp-content/uploads/2015/09/sandwich-club-sandwich.jpg'
               },
               {
 
@@ -261,16 +261,17 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 												},
 
                     image: 'https://thumbs.dreamstime.com/z/grilled-fish-served-potatoes-sauce-lemon-close-up-29801081.jpg'
-                    }]
+            }]
 
 
+//---list of like item
 
 						$scope.lists1 = [
-						{'vl' : 'custard'},
-						{'vl' : 'almond'},
-						{'vl' : 'cream'},
+						{'vl' : 'pastry'},
+						{'vl' : 'cheese'},
+						{'vl' : 'sandwich'},
 						{'vl' : 'strawberry'},
-						{'vl' : 'apple'},
+						{'vl' : 'vegetables'},
 					];
 					$scope.lst1 = [];
 					$scope.change1 = function(check,value){
@@ -281,13 +282,13 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 								}
 					};
 
-					//
+					//----list of not like items
 					$scope.lists2 = [
-					{'vl' : 'pastry'},
-					{'vl' : 'egg'},
+					{'vl' : 'cream'},
 					{'vl' : 'onion'},
 					{'vl' : 'tomato'},
-					{'vl' : 'banana'},
+					{'vl' : ''},
+					{'vl' : ''},
 					];
 					$scope.lst2 = [];
 					$scope.change2 = function(check,value){
@@ -298,7 +299,7 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 							}
 					};
 
-
+//----------to get favourite dish or not favourite----
 								$scope.getFav = function(url) {
 						var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
 										$http({
@@ -324,14 +325,14 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 																if($scope.ingredients.indexOf($scope.lst2[i]) > -1){
 																	// var info1 = "<h2 class='highlight-info'>You will not like this Food</h2>";
 																  console.log("Not Your FAV");
-																	$(".highlight-info").text('You will not like this Food');
+																	$(".highlight-info").text('Not favourite');
 																		 $(".rest-extra").css("background-color" ,"#ea0b0b");
 
 																					break;
 																}
 																// var info2 = "<h2 class='highlight-info'>This is the food You May LIKE</h2>";
 																console.log("Your FAV");
-																$(".highlight-info").text('This is the food You May LIKE');
+																$(".highlight-info").text('Favourite');
 																	$(".rest-extra").css("background-color" ,"#308917");
 																break;
 															 }
@@ -339,7 +340,7 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 															 else {
 																//  var info1 = "<h2 class='highlight-info'>You will not like this Food</h2>";
 																 console.log("Not Your FAV");
-																	$(".highlight-info").text('You will not like this Food');
+																	$(".highlight-info").text('Not favourite');
 																	$(".rest-extra").css("background-color" ,"#ea0b0b");
 
 															 }
