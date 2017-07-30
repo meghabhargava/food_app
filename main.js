ba5be052927f0a8c838ff91ca497fefa4ab41b6a
Megha Bhargava <er.meghabhargava@gmail.com>
2017-07-29 21:51:28 -0700

    add function to link favourite html page and modify list of like and dislike

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $('#user-email').on('input',function() {
 		templateUrl: 'pages/restaurant.html', // calling of url restaurant page by giving parameter
 		controller: 'restaurantController'
 	})
-  .when('/fav', {
+  .when('/fav',{
 		templateUrl: 'pages/favourite.html',
 		controller: 'favController'
 	})
@@ -43,7 +43,7 @@ $('#user-email').on('input',function() {
 })
 
 //-----------restaurant controller-------------------------
-foodieApp.controller('restaurantController',function($scope,$routeParams,$http,$location) {
+foodieApp.controller('restaurantController',function($scope,$routeParams,$location,$http) {
 $scope.ingredients = [];
     $scope.restaurantId = $routeParams.id; //catch the values of restaurants which is given in the url
 
@@ -138,11 +138,13 @@ $http({
     {
         console.log(xhr);
     });
- }
+  }
  $scope.goToFav = function() {
+
 													$location.url('fav')
 													console.log($location.url);
 												}
+
 })
 
 
@@ -216,3 +218,142 @@ $scope.restaurants = [{
 })
 
 // favController
+
+foodieApp.controller('favController',function($scope,$location,$http) {
+
+
+	$scope.ingredients = [];
+
+//console.log($routeParams.id);
+
+  $scope.restaurants = [{
+							id: 1,
+							bestDish: {
+										name: 'Cake',
+										image: 'http://img10.deviantart.net/c2a3/i/2015/165/0/2/small_ckae_by_mariosonicfan16-d8x8b0c.jpg'
+									},
+							image: 'http://img10.deviantart.net/c2a3/i/2015/165/0/2/small_ckae_by_mariosonicfan16-d8x8b0c.jpg'
+						},{
+
+						id: 2,
+						bestDish: {
+									name: 'Corn Pizza',
+									image: 'https://images.food52.com/zyrGNL1_8ZxmJ43jtBLQoyLfNvA=/753x502/c947f605-2d61-4a10-9f69-abc7dda9fffb--DSC07406.JPG'
+								},
+
+            image: 'https://images.food52.com/zyrGNL1_8ZxmJ43jtBLQoyLfNvA=/753x502/c947f605-2d61-4a10-9f69-abc7dda9fffb--DSC07406.JPG'
+          },
+          {
+								id: 3,
+								bestDish: {
+											name: 'Italian Pasta',
+											image: 'https://www.sensibus.com/deli/sites/sensibus.com/files/recipes/pasta-dish-2_0.jpg'
+										},
+
+                image: 'https://www.sensibus.com/deli/sites/sensibus.com/files/recipes/pasta-dish-2_0.jpg'
+              },
+              {
+
+										id: 4,
+										bestDish: {
+													name: 'Grilled fish',
+													image: 'https://thumbs.dreamstime.com/z/grilled-fish-served-potatoes-sauce-lemon-close-up-29801081.jpg'
+												},
+
+                    image: 'https://thumbs.dreamstime.com/z/grilled-fish-served-potatoes-sauce-lemon-close-up-29801081.jpg'
+                    }]
+
+
+
+						$scope.lists1 = [
+						{'vl' : 'custard'},
+						{'vl' : 'almond'},
+						{'vl' : 'cream'},
+						{'vl' : 'strawberry'},
+						{'vl' : 'apple'},
+					];
+					$scope.lst1 = [];
+					$scope.change1 = function(check,value){
+								if(check){
+										$scope.lst1.push(value);
+								}else{
+										 $scope.lst1.splice($scope.lst1.indexOf(value), 1);
+								}
+					};
+
+					//
+					$scope.lists2 = [
+					{'vl' : 'pastry'},
+					{'vl' : 'egg'},
+					{'vl' : 'onion'},
+					{'vl' : 'lettuce'},
+					{'vl' : 'banana'},
+					];
+					$scope.lst2 = [];
+					$scope.change2 = function(check,value){
+							if(check){
+									$scope.lst2.push(value);
+							}else{
+									 $scope.lst2.splice($scope.lst2.indexOf(value), 1);
+							}
+					};
+
+
+								$scope.getFav = function(url) {
+						var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
+										$http({
+											'method': 'POST',
+											'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
+											'headers': {
+												'Authorization': 'Key f49e748db7974ab6831f6c2b210d9cde',
+												'Content-Type': 'application/json'
+											},
+											'data': data,
+
+										}).then(function (response) {
+													var ingredients = response.data.outputs[0].data.concepts;
+										  			var list = '';
+														//  var cboxArray = [];
+														for (var i =0;i < ingredients.length;i++) {
+															$scope.ingredients.push(ingredients[i].name);
+														}
+
+														for(var i=0;i< $scope.lst1.length;i++){
+													if ($scope.ingredients.indexOf($scope.lst1[i]) > -1) {
+
+																if($scope.ingredients.indexOf($scope.lst2[i]) > -1){
+																	// var info1 = "<h2 class='highlight-info'>You will not like this Food</h2>";
+																  console.log("Not Your FAV");
+																	$(".highlight-info").text('You will not like this Food');
+																		 $(".rest-extra").css("background-color" ,"#ea0b0b");
+
+																					break;
+																}
+																// var info2 = "<h2 class='highlight-info'>This is the food You May LIKE</h2>";
+																console.log("Your FAV");
+																$(".highlight-info").text('This is the food You May LIKE');
+																	$(".rest-extra").css("background-color" ,"#308917");
+																break;
+															 }
+
+															 else {
+																//  var info1 = "<h2 class='highlight-info'>You will not like this Food</h2>";
+																 console.log("Not Your FAV");
+																	$(".highlight-info").text('You will not like this Food');
+																	$(".rest-extra").css("background-color" ,"#ea0b0b");
+
+															 }
+
+									}
+
+
+											//console.log(list);
+										}, function (xhr) {
+																	   console.log(xhr);
+																	  });
+																}
+
+
+
+
+})
